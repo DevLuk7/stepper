4c56d30d62c34d35618f3c7847f8263c57d6edac
Luk <lukass100@gmail.com>
2020-04-15 19:39:51 +0200

    create stepper module and component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { StepperModule } from './stepper/stepper.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    StepperModule
   ],
   providers: [],
   bootstrap: [AppComponent]
